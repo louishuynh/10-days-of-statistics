14bb5625c6a233abbad8a93db71e946775ff8f55
louis <@>
2019-07-20 00:04:16 +0100

    Completed basic probability.

---
+++ day2_ex1_basic_probability/solution.py
@@ -0,0 +1,12 @@
+from itertools import product
+from statistics import mean
+
+die_outcomes = [roll + 1 for roll in range(6)]
+
+at_most_nine = ([1 if d1 + d2 <= 9 else 0 for d1, d2 in product(die_outcomes, die_outcomes)])
+print(sum(at_most_nine))
+print(len(at_most_nine))
+# Answer is 30/36 or 5/6
+
+print(mean(at_most_nine))
+
